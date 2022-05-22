e576ebb630d8a36a0404ebffda77208ca4a882be
Mihaly Papp <papp@xund.ai>
2022-05-22 17:17:18 +0200

    feat: added factorial calculations

--- src/handlers/FactorialHandler.hs
+++ src/handlers/FactorialHandler.hs
@@ -1,24 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module HelloHandler (helloHandler) where
+module FactorialHandler (factorialHandler) where
 
 import Data.Maybe
 import qualified Data.Text as T
 import Text.Read
 import Web.Firefly
 
--- | Get the 'name' query param from the url, if it doesn't exist use 'Stranger'
-helloHandler :: Handler T.Text
-helloHandler = do
+factorialHandler :: Handler T.Text
+factorialHandler = do
   pathInfo <- getPathInfo
-  name <- getQuery "name"
 
   case pathInfo of
     ["factorial", _] -> do
-      case readMaybe $ T.unpack $ pathInfo !! 2 of
+      case readMaybe $ T.unpack $ pathInfo !! 1 of
         Just n -> return $ T.pack $ show $ factorialOf n
         Nothing -> return "Invalid number"
       where
         factorialOf n = product [1 .. n]
     _ -> do
-      return $ "Hello " <> fromMaybe "Stranger" name
+      return "No number given"
